c8469e7513572214f07bd21df2d248c6e19bdf99
Momina Shakoor <mominashakoorf@gmail.com>
2024-12-03 16:44:46 +0500

    Added a label to form

--- com.idlogix.forms/src/com/idlogix/form/Form.java
+++ com.idlogix.forms/src/com/idlogix/form/Form.java
@@ -3,29 +3,38 @@ package com.idlogix.form;
 import org.adempiere.webui.panel.ADForm;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zul.Label;
 import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Vbox;
 
 public class Form extends ADForm {
 
-	Textbox b = new Textbox();
-	
-	@Override
-	protected void initForm() {
-		// TODO Auto-generated method stub
-	b.addEventListener(Events.ON_OK, this);
-	this.appendChild(b);
-	
-	}
-	
-	@Override
-	public void onEvent(Event event) throws Exception {
-	    try {
-	        if (event.getTarget().equals(b) && event.getName().equals(Events.ON_OK)) {
-	            System.out.println("Textbox: " + b.getRawValue());
-	        }
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	}
+    Textbox b = new Textbox();
+    Label label = new Label("Enter Text:");
 
+    @Override
+    protected void initForm() {
+        Vbox container = new Vbox();
+        container.setSpacing("10px"); 
+        
+        
+        container.appendChild(label);
+        container.appendChild(b);
+
+       
+        b.addEventListener(Events.ON_OK, this);
+
+        this.appendChild(container);
+    }
+
+    @Override
+    public void onEvent(Event event) throws Exception {
+        try {
+            if (event.getTarget().equals(b) && event.getName().equals(Events.ON_OK)) {
+                System.out.println("Textbox: " + b.getRawValue());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
